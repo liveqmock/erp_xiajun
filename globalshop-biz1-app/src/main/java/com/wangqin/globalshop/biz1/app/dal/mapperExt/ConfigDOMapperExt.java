2666a043974f64b1eaba9395bb3ebc28193ed8cf
johnnyxiong <xiongjy2104@gmail.com>
2018-05-23 16:23:09 +0800

    merge old usercenter

--- globalshop-biz1-app/src/main/java/com/wangqin/globalshop/biz1/app/dal/mapperExt/ConfigDOMapperExt.java
+++ globalshop-biz1-app/src/main/java/com/wangqin/globalshop/biz1/app/dal/mapperExt/ConfigDOMapperExt.java
@@ -1,10 +1,10 @@
 package com.wangqin.globalshop.biz1.app.dal.mapperExt;
 
-
-import com.wangqin.globalshop.biz1.app.dal.mapper.ConfigDOMapper;
-
-/**
- * Created by Patrick on 2018/5/15.
- */
-public interface ConfigDOMapperExt  extends ConfigDOMapper {
-}
+//
+//import com.wangqin.globalshop.biz1.app.dal.mapper.ConfigDOMapper;
+//
+///**
+// * Created by Patrick on 2018/5/15.
+// */
+//public interface ConfigDOMapperExt  extends ConfigDOMapper {
+//}
--- globalshop-web/src/test/java/com/wangqin/globalshop/web/dal/mapper/ConfigDOMapperTest.java
+++ globalshop-web/src/test/java/com/wangqin/globalshop/web/dal/mapper/ConfigDOMapperTest.java
@@ -1,7 +1,7 @@
 package com.wangqin.globalshop.web.dal.mapper;
 
-import com.wangqin.globalshop.biz1.app.dal.dataObject.ConfigDO;
-import com.wangqin.globalshop.biz1.app.dal.mapperExt.ConfigDOMapperExt;
+//import com.wangqin.globalshop.biz1.app.dal.dataObject.ConfigDO;
+//import com.wangqin.globalshop.biz1.app.dal.mapperExt.ConfigDOMapperExt;
 import com.wangqin.globalshop.web.TST;
 import com.wangqin.globalshop.common.utils.LogWorker;
 import com.wangqin.globalshop.common.utils.StringHelper;
@@ -15,37 +15,37 @@ import javax.annotation.Resource;
  */
 @Slf4j
 public class ConfigDOMapperTest extends TST {
-    @Resource
-    ConfigDOMapperExt configDOMapperExt;
-    @Test
-    public void testDeleteByPrimaryKey() throws Exception {
-
-    }
-
-    @Test
-    public void testInsert() throws Exception {
-
-    }
-
-    @Test
-    public void testInsertSelective() throws Exception {
-
-    }
-
-    @Test
-    public void testSelectByPrimaryKey() throws Exception {
-        ConfigDO configDO = configDOMapperExt.selectByPrimaryKey(1L);
-        LogWorker.log(log,"查询",":{}", StringHelper.json(configDO));
-    }
-
-    @Test
-    public void testUpdateByPrimaryKeySelective() throws Exception {
-
-    }
-
-    @Test
-    public void testUpdateByPrimaryKey() throws Exception {
-
-    }
+//    @Resource
+//    ConfigDOMapperExt configDOMapperExt;
+//    @Test
+//    public void testDeleteByPrimaryKey() throws Exception {
+//
+//    }
+//
+//    @Test
+//    public void testInsert() throws Exception {
+//
+//    }
+//
+//    @Test
+//    public void testInsertSelective() throws Exception {
+//
+//    }
+//
+//    @Test
+//    public void testSelectByPrimaryKey() throws Exception {
+//        ConfigDO configDO = configDOMapperExt.selectByPrimaryKey(1L);
+//        LogWorker.log(log,"查询",":{}", StringHelper.json(configDO));
+//    }
+//
+//    @Test
+//    public void testUpdateByPrimaryKeySelective() throws Exception {
+//
+//    }
+//
+//    @Test
+//    public void testUpdateByPrimaryKey() throws Exception {
+//
+//    }
 
 }
