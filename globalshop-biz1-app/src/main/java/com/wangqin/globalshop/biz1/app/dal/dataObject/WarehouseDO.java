5de15db3793195e7ca93e4476d5eb66f8003fbc3
johnnyxiong <xiongjy2104@gmail.com>
2018-05-29 14:52:22 +0800

    add warehouse mapper

---
+++ globalshop-biz1-app/src/main/java/com/wangqin/globalshop/biz1/app/dal/dataObject/WarehouseDO.java
@@ -0,0 +1,135 @@
+package com.wangqin.globalshop.biz1.app.dal.dataObject;
+
+import java.util.Date;
+
+public class WarehouseDO {
+    private Long id;
+
+    private String warehouseNo;
+
+    private String name;
+
+    private String companyNo;
+
+    private Integer deliveryPriority;
+
+    private String address;
+
+    private String tel;
+
+    private String contactPerson;
+
+    private Date gmtCreate;
+
+    private Date gmtModify;
+
+    private Boolean isDel;
+
+    private String creator;
+
+    private String modifier;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getWarehouseNo() {
+        return warehouseNo;
+    }
+
+    public void setWarehouseNo(String warehouseNo) {
+        this.warehouseNo = warehouseNo == null ? null : warehouseNo.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getCompanyNo() {
+        return companyNo;
+    }
+
+    public void setCompanyNo(String companyNo) {
+        this.companyNo = companyNo == null ? null : companyNo.trim();
+    }
+
+    public Integer getDeliveryPriority() {
+        return deliveryPriority;
+    }
+
+    public void setDeliveryPriority(Integer deliveryPriority) {
+        this.deliveryPriority = deliveryPriority;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address == null ? null : address.trim();
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel == null ? null : tel.trim();
+    }
+
+    public String getContactPerson() {
+        return contactPerson;
+    }
+
+    public void setContactPerson(String contactPerson) {
+        this.contactPerson = contactPerson == null ? null : contactPerson.trim();
+    }
+
+    public Date getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Date gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public Date getGmtModify() {
+        return gmtModify;
+    }
+
+    public void setGmtModify(Date gmtModify) {
+        this.gmtModify = gmtModify;
+    }
+
+    public Boolean getIsDel() {
+        return isDel;
+    }
+
+    public void setIsDel(Boolean isDel) {
+        this.isDel = isDel;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator == null ? null : creator.trim();
+    }
+
+    public String getModifier() {
+        return modifier;
+    }
+
+    public void setModifier(String modifier) {
+        this.modifier = modifier == null ? null : modifier.trim();
+    }
+}
---
+++ globalshop-biz1-app/src/main/java/com/wangqin/globalshop/biz1/app/dal/mapper/WarehouseDOMapper.java
@@ -0,0 +1,17 @@
+package com.wangqin.globalshop.biz1.app.dal.mapper;
+
+import com.wangqin.globalshop.biz1.app.dal.dataObject.WarehouseDO;
+
+public interface WarehouseDOMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(WarehouseDO record);
+
+    int insertSelective(WarehouseDO record);
+
+    WarehouseDO selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(WarehouseDO record);
+
+    int updateByPrimaryKey(WarehouseDO record);
+}
