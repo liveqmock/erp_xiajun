231b9ab4b87647b8b0425a8f17d0b1fc38c15e31
Biscuits <435866911@qq.com>
2018-06-01 13:52:13 +0800

    ,,

--- globalshop-biz1-app/src/main/java/com/wangqin/globalshop/biz1/app/dal/mapper/WarehouseDOMapper.java
+++ globalshop-biz1-app/src/main/java/com/wangqin/globalshop/biz1/app/dal/mapper/WarehouseDOMapper.java
@@ -1,8 +1,11 @@
 package com.wangqin.globalshop.biz1.app.dal.mapper;
 
-
 import com.wangqin.globalshop.biz1.app.dal.dataObject.WarehouseDO;
 
-public interface WareHouseDOMapper {
+/**
+ * @author biscuit
+ * @data 2018/06/01
+ */
+public interface WarehouseDOMapper {
     WarehouseDO selectByWarehourseNo(String warehouseNo);
 }
