1e728dcd874af8a304eac45248667ac859c60b9d
zyfjxust@163.com <zyfjxust@163.com>
2018-06-01 18:57:04 +0800

    channellistItemsku

--- globalshop-biz1-app/src/main/java/com/wangqin/globalshop/biz1/app/dal/mapperExt/ChannelListingItemSkuDOMapperExt.java
+++ globalshop-biz1-app/src/main/java/com/wangqin/globalshop/biz1/app/dal/mapperExt/ChannelListingItemSkuDOMapperExt.java
@@ -4,5 +4,6 @@ import com.wangqin.globalshop.biz1.app.dal.dataObject.ChannelListingItemSkuDO;
 import com.wangqin.globalshop.biz1.app.dal.mapper.ChannelListingItemSkuDOMapper;
 
 public interface ChannelListingItemSkuDOMapperExt extends ChannelListingItemSkuDOMapper {
+
     ChannelListingItemSkuDO queryPo(ChannelListingItemSkuDO so);
 }
