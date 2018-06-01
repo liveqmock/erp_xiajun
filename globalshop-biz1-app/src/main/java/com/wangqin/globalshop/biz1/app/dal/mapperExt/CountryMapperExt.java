c9d34f58efe0563a3c246e9d82ee4c0d51079914
sivan <sivan@logthin.com>
2018-06-01 10:55:50 +0800

    no message

--- globalshop-biz1-app/src/main/java/com/wangqin/globalshop/biz1/app/dal/mapperExt/CountryMapperExt.java
+++ globalshop-biz1-app/src/main/java/com/wangqin/globalshop/biz1/app/dal/mapperExt/CountryMapperExt.java
@@ -1,5 +1,7 @@
 package com.wangqin.globalshop.biz1.app.dal.mapperExt;
 
+import java.util.List;
+
 import com.wangqin.globalshop.biz1.app.dal.dataObject.CountryDO;
 import com.wangqin.globalshop.biz1.app.dal.mapper.CountryDOMapper;
 
