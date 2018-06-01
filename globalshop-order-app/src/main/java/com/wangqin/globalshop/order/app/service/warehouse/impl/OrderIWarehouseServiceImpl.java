10400d0a2e0a495e6fb885f4385a39799ec3fce5
Biscuits <435866911@qq.com>
2018-06-01 13:59:15 +0800

    ,,

--- globalshop-order-app/src/main/java/com/wangqin/globalshop/order/app/service/warehouse/impl/OrderIWarehouseServiceImpl.java
+++ globalshop-order-app/src/main/java/com/wangqin/globalshop/order/app/service/warehouse/impl/OrderIWarehouseServiceImpl.java
@@ -1,7 +1,6 @@
 package com.wangqin.globalshop.order.app.service.warehouse.impl;
 
 import com.wangqin.globalshop.biz1.app.dal.dataObject.WarehouseDO;
-import com.wangqin.globalshop.biz1.app.dal.mapper.WarehouseDOMapper;
 import com.wangqin.globalshop.biz1.app.dal.mapperExt.WarehouseDOMapperExt;
 import com.wangqin.globalshop.order.app.service.warehouse.OrderIWarehouseService;
 import org.springframework.beans.factory.annotation.Autowired;
