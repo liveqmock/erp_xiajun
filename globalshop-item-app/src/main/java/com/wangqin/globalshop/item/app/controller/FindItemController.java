46f1b25eb516b82558505b20818ca341c24e25c3
brucexiajun <brucexiajun@126.com>
2018-06-07 19:33:25 +0800

    添加发现商品

---
+++ globalshop-item-app/src/main/java/com/wangqin/globalshop/item/app/controller/FindItemController.java
@@ -0,0 +1,49 @@
+package com.wangqin.globalshop.item.app.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.wangqin.globalshop.biz1.app.dal.dataObject.ItemFindDO;
+import com.wangqin.globalshop.biz1.app.vo.ItemQueryVO;
+import com.wangqin.globalshop.common.utils.EasyuiJsonResult;
+import com.wangqin.globalshop.common.utils.JsonPageResult;
+import com.wangqin.globalshop.item.app.service.IFindItemService;
+
+
+/**
+ * 采购商品处理器
+ * @author wangkan
+ *
+ */
+@Controller
+@RequestMapping("/item/finditem")
+public class FindItemController {
+	@Autowired
+	private IFindItemService findItemService;
+	/**
+	 * 商品查询管理主列表
+	 * @return
+	 */
+	@RequestMapping("/queryFindItemList")
+	@ResponseBody
+	public Object queryItemList(ItemQueryVO itemQueryVO) {
+		JsonPageResult<List<ItemFindDO>> result = findItemService.queryFindItems(itemQueryVO);
+		EasyuiJsonResult<List<ItemFindDO>> jsonResult = new EasyuiJsonResult<>();
+		jsonResult.setTotal(result.getTotalCount());
+		jsonResult.setRows(result.getData());
+		return jsonResult.buildIsSuccess(true);
+	}
+	/**
+	 * 单个采购商品通过或拒绝
+	 * @param item
+	 */
+//	@RequestMapping("/passOrRefuse")
+//	@ResponseBody
+//	public Object passOrRefuse(FindItem item) {
+//		return findItemService.passOrRefuse(item);
+//	}
+}
