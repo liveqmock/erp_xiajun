df5547d60d9b87edf8b7094e342cb10127926ea0
Biscuits <435866911@qq.com>
2018-06-07 11:03:38 +0800

    登陆页面

--- globalshop-usercenter-app/src/main/java/com/wangqin/globalshop/usercenter/controller/HaiLoginController.java
+++ globalshop-usercenter-app/src/main/java/com/wangqin/globalshop/usercenter/controller/HaiLoginController.java
@@ -1,21 +1,22 @@
 package com.wangqin.globalshop.usercenter.controller;
 
+import com.wangqin.globalshop.biz1.app.dal.dataObject.AuthUserDO;
 import com.wangqin.globalshop.common.base.BaseController;
-import com.wangqin.globalshop.common.csrf.CsrfToken;
 import com.wangqin.globalshop.common.redis.Cache;
 import com.wangqin.globalshop.common.utils.AppUtil;
-import com.wangqin.globalshop.common.utils.JsonResult;
+import com.wangqin.globalshop.common.utils.StringUtils;
 import com.wangqin.globalshop.usercenter.service.IUserService;
+import com.wangqin.globalshop.usercenter.vo.UserVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
 /**
- * 
  * @author zhulu
- *
  */
 @Controller
 public class HaiLoginController extends BaseController {
@@ -25,70 +26,64 @@ public class HaiLoginController extends BaseController {
 
     public static final String COMPANY_NO = "CompanyNO_";
 
-    private static long  TIMEOUT=30*60*1000;
+    private static long TIMEOUT = 30 * 60 * 1000;
     @Autowired
     IUserService userService;
 
     @Autowired
     Cache loginCache;
-	
-	  /**
+
+    /**
      * POST 登录 shiro 写法
      *
      * @param username 用户名
      * @param password 密码
      * @return {Object}
      */
-//    @PostMapping("/haiLogin")
-	@RequestMapping(value = "/haiLogin/login",method = RequestMethod.POST)
-    @CsrfToken(remove = true)
+    @PostMapping("/haiLogin/login")
+//    @RequestMapping("/login")
     @ResponseBody
-    public Object loginPost(HttpServletRequest request, String username, String password,
+    public Object loginPost(HttpServletRequest request, String username, String password, String captcha,
                             @RequestParam(value = "rememberMe", defaultValue = "1") Integer rememberMe) {
         logger.info("POST请求登录");
-        JsonResult<Object> jsonResult = new JsonResult<>();
-        return jsonResult.buildIsSuccess(true);
-//        // 改为全部抛出异常，避免ajax csrf token被刷新
-//        if (StringUtils.isBlank(username)) {
-//            throw new RuntimeException("用户名不能为空");
-//        }
-//        if (StringUtils.isBlank(password)) {
-//            throw new RuntimeException("密码不能为空");
-//        }
-//        Subject user = SecurityUtils.getSubject();
-//        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
-//        // 设置记住密码
-//        token.setRememberMe(1 == rememberMe);
-//        JsonResult<String> result = new JsonResult<>();
-//        try {
-//            user.login(token);
-//            return result.buildIsSuccess(true);
-//        } catch (UnknownAccountException e) {
-//        	e.printStackTrace();
-//        	return result.buildData("账号不存在！").buildIsSuccess(false);
-////            throw new RuntimeException("账号不存在！", e);
-//        } catch (DisabledAccountException e) {
-////            throw new RuntimeException("账号未启用！", e);
-//        	e.printStackTrace();
-//        	return result.buildData("账号未启用！").buildIsSuccess(false);
-//        } catch (IncorrectCredentialsException e) {
-////            throw new RuntimeException("密码错误！", e);
-//            e.printStackTrace();
-//        	return result.buildData("密码错误！").buildIsSuccess(false);
-//        } catch (Throwable e) {
-//            throw new RuntimeException(e.getMessage(), e);
-//        }
+        // 改为全部抛出异常，避免ajax csrf token被刷新
+        if (StringUtils.isBlank(username)) {
+            throw new RuntimeException("用户名不能为空");
+        }
+        if (StringUtils.isBlank(password)) {
+            throw new RuntimeException("密码不能为空");
+        }
+        UserVo userVo = new UserVo();
+        userVo.setLoginName(username);
+        AuthUserDO userDOList = userService.selectByLoginName(username);
+        if (userDOList == null) {
+            return renderError("该用户不存在");
+        } else {
+            //只会有一个，多了需要检查数据库约束
+            AuthUserDO user = userDOList;
+            String sessionId = (String) request.getAttribute(SESSION_ID);
+            if (StringUtils.isNotBlank(sessionId)) {
+                loginCache.putEx(sessionId, username, TIMEOUT);
+                loginCache.putEx(COMPANY_NO + sessionId, user.getCompanyNo(), TIMEOUT);
+                AppUtil.setLoginUser(username, user.getCompanyNo());
+                return renderSuccess();
+            } else {
+                return renderError("sessionId不存在");
+            }
+
+        }
     }
-	
-	
-	
+
+
 //	@RequestMapping("/web")
 //	public String agent(Model model) {
 //		model.addAttribute("version", "1111");
 //		return "agent";
 //	}
+
     /**
      * 退出
+     *
      * @return {Result}
      */
     @PostMapping("/logout")
@@ -99,7 +94,7 @@ public class HaiLoginController extends BaseController {
         loginCache.remove(sessionId);
         AppUtil.removeLoginUserId();
 
-        loginCache.remove(COMPANY_NO+sessionId);
+        loginCache.remove(COMPANY_NO + sessionId);
         AppUtil.removeLoginUserId();
         return renderSuccess();
     }
