25d27a9e442708831902af99ad9ff0503de54a67
brucexiajun <brucexiajun@126.com>
2018-06-12 10:27:40 +0800

    删除商品模块旧的渠道部分，注释掉采购模块有错的代码，判断能否从Apputil获得登录人和公司信息，不能用固定值代替

--- globalshop-purchase/src/main/java/com/wangqin/globalshop/purchase/service/IReceiptService.java
+++ globalshop-purchase/src/main/java/com/wangqin/globalshop/purchase/service/IReceiptService.java
@@ -1,12 +1,12 @@
-package com.wangqin.globalshop.purchase.service;
-
-import com.wangqin.globalshop.biz1.app.dal.dataObject.BuyerReceiptDO;
-import com.wangqin.globalshop.biz1.app.dal.dataVo.ReceiptQueryVO;
-import com.wangqin.globalshop.common.utils.JsonPageResult;
-
-import java.util.List;
-
-public interface IReceiptService   {
-//	void createReceipt(BuyerReceiptDO receipt);
-	JsonPageResult<List<BuyerReceiptDO>> queryReceipt(ReceiptQueryVO receiptQueryVO);
-}
+//package com.wangqin.globalshop.purchase.service;
+//
+//import com.wangqin.globalshop.biz1.app.dal.dataObject.BuyerReceiptDO;
+//import com.wangqin.globalshop.biz1.app.dal.dataVo.ReceiptQueryVO;
+//import com.wangqin.globalshop.common.utils.JsonPageResult;
+//
+//import java.util.List;
+//
+//public interface IReceiptService   {
+////	void createReceipt(BuyerReceiptDO receipt);
+//	JsonPageResult<List<BuyerReceiptDO>> queryReceipt(ReceiptQueryVO receiptQueryVO);
+//}
