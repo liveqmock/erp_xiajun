f838dc291fa77e7d39be6cc1fea23a60264d56ed
zyfjxust@163.com <zyfjxust@163.com>
2018-06-13 16:33:54 +0800

    京东task,抓商品，抓订单，推商品下来，推订单下来，含failure

---
+++ globalshop-channel/src/main/java/com/wangqin/globalshop/channel/service/jingdong/GlobalshopStatic.java
@@ -0,0 +1,17 @@
+package com.wangqin.globalshop.channel.service.jingdong;
+
+/**
+ * 放些向globalshop的资源
+ *
+ * Create by 777 on 2018/6/13
+ */
+public class GlobalshopStatic {
+
+	public static final String globalshop_dev_url = "http://test.buyer007.cn";//网擒开发地址
+
+	public static final String globalshop_uat_url = "http://test.buyer007.cn";//网擒测试地址
+
+	public static final String globalshop_prod_url = "http://test.buyer007.cn";//网擒生产地址
+
+
+}
---
+++ globalshop-channel/src/main/java/com/wangqin/globalshop/channel/service/jingdong/SendItemType.java
@@ -0,0 +1,14 @@
+package com.wangqin.globalshop.channel.service.jingdong;
+
+/**
+ * 下发商品至globalshop的类型
+ * Create by 777 on 2018/6/13
+ */
+public class SendItemType {
+
+	public static final String ITEM_FROM_ADD = "ITEM_FROM_ADD";//上新时返回的商品数据
+
+	public static final String ITEM_FROM_TASK = "ITEM_FROM_TASK"; //
+
+
+}
