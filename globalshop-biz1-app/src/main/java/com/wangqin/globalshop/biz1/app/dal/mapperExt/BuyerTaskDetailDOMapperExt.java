bbaf2fdc9aae25fc057d71df57f2491ff71bbc24
biscuits <435866911@qq.com>
2018-06-19 16:38:53 +0800

    新增导入采购任务功能

--- globalshop-biz1-app/src/main/java/com/wangqin/globalshop/biz1/app/dal/mapperExt/BuyerTaskDetailDOMapperExt.java
+++ globalshop-biz1-app/src/main/java/com/wangqin/globalshop/biz1/app/dal/mapperExt/BuyerTaskDetailDOMapperExt.java
@@ -1,6 +1,10 @@
 package com.wangqin.globalshop.biz1.app.dal.mapperExt;
 
+import com.wangqin.globalshop.biz1.app.dal.dataObject.BuyerTaskDetailDO;
 import com.wangqin.globalshop.biz1.app.dal.mapper.BuyerTaskDetailDOMapper;
 
+import java.util.List;
+
 public interface BuyerTaskDetailDOMapperExt extends BuyerTaskDetailDOMapper {
+    void inserBatch(List<BuyerTaskDetailDO> detailList);
 }
