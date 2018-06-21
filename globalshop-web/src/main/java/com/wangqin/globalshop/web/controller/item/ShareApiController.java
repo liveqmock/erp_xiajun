8c289887b65ccfb166c09a09f29c7af45b8b93ca
brucexiajun <brucexiajun@126.com>
2018-06-21 13:43:40 +0800

    修复ShareApiController错误

--- globalshop-web/src/main/java/com/wangqin/globalshop/web/controller/item/ShareApiController.java
+++ globalshop-web/src/main/java/com/wangqin/globalshop/web/controller/item/ShareApiController.java
@@ -1,8 +1,8 @@
 package com.wangqin.globalshop.web.controller.item;
 
+import com.wangqin.globalshop.common.base.BaseDto;
 import com.wangqin.globalshop.common.utils.DimensionCodeUtil;
 import com.wangqin.globalshop.common.utils.JsonResult;
-import com.wangqin.globalshop.common.base.BaseDto;
 import com.wangqin.globalshop.item.app.service.IItemService;
 import com.wangqin.globalshop.web.dto.api.ItemShareEntity;
 import net.sf.json.JSONObject;
@@ -29,8 +29,6 @@ public class ShareApiController {
     public static final String ACCESS_TOKENURL = "https://api.weixin.qq.com/cgi-bin/token";
     public static final String ACCESS_TOKENPARAM = "grant_type=client_credential&appid=wxdef3e972a4a93e91&secret=fef11f402f8e8f3c1442163155aeb65a";
 
-	@Autowired
-	private IItemService itemService;
     @RequestMapping("/api/items/share")
     @ResponseBody
     public String detail(@RequestParam("itemCode") String itemCode,
@@ -51,23 +49,23 @@ public class ShareApiController {
 
         entity.setItemImgList(Arrays.asList(picUrl));
 
-//        ItemShareEntity itemShareEntity = new ItemShareEntity();
-//
-//        String pic = itemService.queryItemPicByItemCodeAndCompanyNo(itemCode, companyNo);
-//        String desc = itemService.itemDetailByItemCode(itemCode, companyNo).getDetail();
-//
-//        itemShareEntity.setItemDesc(desc);
-//        JSONObject jsonObject = JSONObject.fromObject(pic);
-//        int num = Integer.parseInt(jsonObject.getString("mainPicNum"));
-//        JSONArray jsonArray = JSONArray.fromObject(jsonObject.getString("picList"));
-//        List<String> picList = new ArrayList<>();
-//        for(int i = 0; i < jsonArray.size(); i ++) {
-//        	JSONObject jsonPicList = jsonArray.getJSONObject(i);
-//        	picList.add((String) jsonPicList.get("uid"));
-//        }
-//        if(num > 8) {
-//
-//        }
+        ItemShareEntity itemShareEntity = new ItemShareEntity();
+
+        String pic = itemService.queryItemPicByItemCodeAndCompanyNo(itemCode, companyNo);
+        String desc = itemService.itemDetailByItemCode(itemCode, companyNo).getDetail();
+
+        itemShareEntity.setItemDesc(desc);
+        JSONObject jsonObject = JSONObject.fromObject(pic);
+        int num = Integer.parseInt(jsonObject.getString("mainPicNum"));
+        JSONArray jsonArray = JSONArray.fromObject(jsonObject.getString("picList"));
+        List<String> picList = new ArrayList<>();
+        for(int i = 0; i < jsonArray.size(); i ++) {
+        	JSONObject jsonPicList = jsonArray.getJSONObject(i);
+        	picList.add((String) jsonPicList.get("uid"));
+        }
+        if(num > 8) {
+
+        }
 
         jsonResult.buildIsSuccess(true).buildData(entity);
         return BaseDto.toString(jsonResult);
