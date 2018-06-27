79d94b451b27250b413c03dce824ec1cefaff891
zhangziyang <378659373@qq.com>
2018-06-27 11:08:32 +0800

    客户管理搜索BUG

--- globalshop-biz1-app/src/main/java/com/wangqin/globalshop/biz1/app/dal/mapperExt/DealerDOMapperExt.java
+++ globalshop-biz1-app/src/main/java/com/wangqin/globalshop/biz1/app/dal/mapperExt/DealerDOMapperExt.java
@@ -6,6 +6,7 @@ import com.wangqin.globalshop.biz1.app.dal.mapper.DealerDOMapper;
 import java.util.List;
 import java.util.Map;
 
+
 public interface DealerDOMapperExt extends DealerDOMapper {
     List<Map<String, String>> dealerList(DealerDO seller);
     
