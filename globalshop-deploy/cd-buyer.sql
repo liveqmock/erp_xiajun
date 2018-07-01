82a30e4e84daf402b3c1eb330db1ad924bfb3a78
johnnyxiong <xiongjy2104@gmail.com>
2018-07-01 19:10:34 +0800

    update sql script

--- globalshop-deploy/cd-buyer.sql
+++ globalshop-deploy/cd-buyer.sql
@@ -1,39 +1,27 @@
 ##1 buyer_entry_manifest
-
 ALTER TABLE `buyer_entry_manifest`
 ADD COLUMN `company_no`  varchar(64) NULL AFTER `id`;
 
 ##2 buyer_receipt
-
 ALTER TABLE `buyer_receipt`
-CHANGE COLUMN `buyer_id` `open_id`  varchar(64) NULL DEFAULT NULL COMMENT 'ÂòÊÖID' AFTER `status`;
+CHANGE COLUMN `buyer_id` `open_id`  varchar(64) NULL DEFAULT NULL COMMENT 'ä¹°æ‰‹ID' AFTER `status`;
 
-##3 buyer_storage  ×îºÃ¼ÓÉÏ±¸×¢£ºCOMMENT '-1¹Ø±Õ£¬0ĞÂ½¨£¬1ÒÑÈ·ÈÏÈë¿â£¬2.³É¹¦£¬3Èë¿âÖĞ'
-
+##3 buyer_storage  æœ€å¥½åŠ ä¸Šå¤‡æ³¨ï¼šCOMMENT '-1å…³é—­ï¼Œ0æ–°å»ºï¼Œ1å·²ç¡®è®¤å…¥åº“ï¼Œ2.æˆåŠŸï¼Œ3å…¥åº“ä¸­'
 ALTER TABLE `buyer_storage`
 ADD COLUMN `status`  int(4) NULL AFTER `modifier`;
 
 ##4 buyer_storage_detail
-
 ALTER TABLE `buyer_storage_detail`
-
 MODIFY COLUMN `item_code`  varchar(64) NULL DEFAULT NULL COMMENT 'itemcode' AFTER `buyer_task_detail_no`;
-
 ADD COLUMN `status`  int(4) NULL AFTER `modifier`;
-
 ADD COLUMN `mem`  varchar(1024) NULL AFTER `status`;
-
 ADD COLUMN `op_user_no`  varchar(64) NULL AFTER `mem`;
-
 ADD COLUMN `op_time`  datetime NULL AFTER `op_user_no`;
 
 ##4 buyer_task_detail
-
 ALTER TABLE `buyer_task_detail`
-MODIFY COLUMN `buyer_open_id`  varchar(64) NULL DEFAULT NULL COMMENT 'ÂòÊÖÎ¢ĞÅID' AFTER `buyer_name`;
-
-CHANGE COLUMN `desc` `remark`  varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'ËµÃ÷' AFTER `sku_code`;
-
+MODIFY COLUMN `buyer_open_id`  varchar(64) NULL DEFAULT NULL COMMENT 'ä¹°æ‰‹å¾®ä¿¡ID' AFTER `buyer_name`;
+CHANGE COLUMN `desc` `remark`  varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'è¯´æ˜' AFTER `sku_code`;
 ADD COLUMN `company_no`  varchar(64) NULL AFTER `buyer_task_detail_no`;
 
 ##5 dealer_type
@@ -41,10 +29,8 @@ ALTER TABLE `dealer_type`
 ADD COLUMN `company_no`  varchar(64) NULL AFTER `modifier`;
 
 ##6 item_find
-
 ALTER TABLE `item_find`
-CHANGE COLUMN `desc` `desc_msg`  varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'ÉÌÆ·ÃèÊöĞÅÏ¢' AFTER `status`;
-
+CHANGE COLUMN `desc` `desc_msg`  varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'å•†å“æè¿°ä¿¡æ¯' AFTER `status`;
 ADD COLUMN `company_no`  varchar(64) NULL AFTER `id`;
 
 
