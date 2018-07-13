2f1b07fc719768c06f27e6759d531a9a38a1da21
Angus Liu <anguang.liu@buyer007.com>
2018-07-13 15:49:23 +0800

    优化库存管理，确认出库单管理逻辑

--- globalshop-biz1-app/src/main/java/com/wangqin/globalshop/biz1/app/dal/mapperExt/InventoryOutManifestMapperExt.java
+++ globalshop-biz1-app/src/main/java/com/wangqin/globalshop/biz1/app/dal/mapperExt/InventoryOutManifestMapperExt.java
@@ -3,13 +3,12 @@ package com.wangqin.globalshop.biz1.app.dal.mapperExt;
 import java.util.List;
 
 import com.wangqin.globalshop.biz1.app.dal.dataObject.InventoryOutManifestDO;
+import com.wangqin.globalshop.biz1.app.dal.mapper.InventoryOutManifestDOMapper;
 import com.wangqin.globalshop.biz1.app.vo.InventoryOutVO;
 
-public interface InventoryOutManifestMapperExt {
+public interface InventoryOutManifestMapperExt extends InventoryOutManifestDOMapper {
 
 	Integer queryInventoryOutCount(InventoryOutVO inventoryOutVO);
 	
 	List<InventoryOutManifestDO> queryInventoryOutList(InventoryOutVO inventoryOutVO);
-
-//	InventoryOutManifestDO insertInventoryOutManifest(InventoryOutManifestVO inventoryOutManifestDO);
 }
