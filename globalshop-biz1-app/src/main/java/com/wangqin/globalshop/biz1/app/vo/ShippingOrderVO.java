b5ca2c3dc43e5e7c5207adb35165f67d96b591bf
Angus Liu <anguang.liu@buyer007.com>
2018-07-17 14:49:50 +0800

    优化发货单管理查询逻辑

--- globalshop-biz1-app/src/main/java/com/wangqin/globalshop/biz1/app/vo/ShippingOrderVO.java
+++ globalshop-biz1-app/src/main/java/com/wangqin/globalshop/biz1/app/vo/ShippingOrderVO.java
@@ -9,94 +9,151 @@ import java.util.Date;
  * @author biscuit
  * @data 2018/05/28
  */
-public class ShippingOrderVO extends ShippingOrderDO {
-    /**包裹号*/
+public class ShippingOrderVO {
+
+    /**
+     * 发货单号
+     */
     private String shippingNo;
-//    /**物流单号*/
-//    private String logisticNo;
-    /**物流公司*/
+
+    /**
+     * 物流运单号
+     */
+    private String logisticNo;
+
+    /**
+     * 物流公司名称
+     */
     private String logisticCompany;
-//    private String erpNo;
-    /**收货人*/
-    private String receiver;
-    /**电话*/
-    private String telephone;
-    //状态
-    private Integer status;
+
+    /**
+     * 快递渠道
+     */
     private Integer type;
-    //发货时间
+    /**
+     * 物流状态/运单状态
+     */
+    private Integer status;
+
+    /**
+     * 收件人
+     */
+    private String receiver;
+
+    /**
+     * 联系电话
+     */
+    private String telephone;
+
+    /**
+     * 发货时间
+     */
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date startOrderTime;
+
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date endOrderTime;
-    private String companyNo;
-    /**
-     * 销售ID
-     */
-    private Integer salesId;
 
-    private String customer_no;
+    private Long id;
+
+    private Integer transferStatus;
 
     private Integer syncSendStatus;
 
-    @Override
+    private String tplPkgStatus;
+
+    private Double freight;
+
+    private Double skuWeight;
+
+    private String mallOrders;
+
+    private String shippingPrinter;
+
+    private String companyNo;
+
+    private String glsReturnBack;
+
+    private String receiverState;
+
+    private String receiverCity;
+
+    private String receiverDistrict;
+
+    private String address;
+
+    private String postcode;
+
+    private String memo;
+
+    private String idCard;
+
+    private String idCardBack;
+
+    private String idCardFront;
+
+    private String creator;
+
+    private String modifier;
+
+    private String logisticType;
+
+    private String salesId;
+
     public String getShippingNo() {
         return shippingNo;
     }
 
-    @Override
     public void setShippingNo(String shippingNo) {
         this.shippingNo = shippingNo;
     }
 
-    @Override
+    public String getLogisticNo() {
+        return logisticNo;
+    }
+
+    public void setLogisticNo(String logisticNo) {
+        this.logisticNo = logisticNo;
+    }
+
     public String getLogisticCompany() {
         return logisticCompany;
     }
 
-    @Override
     public void setLogisticCompany(String logisticCompany) {
         this.logisticCompany = logisticCompany;
     }
 
-    @Override
-    public String getReceiver() {
-        return receiver;
-    }
-
-    @Override
-    public void setReceiver(String receiver) {
-        this.receiver = receiver;
-    }
-
-    @Override
-    public String getTelephone() {
-        return telephone;
-    }
-
-    @Override
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-
-    @Override
-    public Integer getStatus() {
-        return status;
-    }
-
-    @Override
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-
-    @Override
     public Integer getType() {
         return type;
     }
 
-    @Override
     public void setType(Integer type) {
         this.type = type;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 
     public Date getStartOrderTime() {
@@ -115,39 +172,187 @@ public class ShippingOrderVO extends ShippingOrderDO {
         this.endOrderTime = endOrderTime;
     }
 
-    @Override
-    public String getCompanyNo() {
-        return companyNo;
+    public Long getId() {
+        return id;
     }
 
-    @Override
-    public void setCompanyNo(String companyNo) {
-        this.companyNo = companyNo;
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public Integer getSalesId() {
-        return salesId;
+    public Integer getTransferStatus() {
+        return transferStatus;
     }
 
-    public void setSalesId(Integer salesId) {
-        this.salesId = salesId;
+    public void setTransferStatus(Integer transferStatus) {
+        this.transferStatus = transferStatus;
     }
 
-    public String getCustomer_no() {
-        return customer_no;
-    }
-
-    public void setCustomer_no(String customer_no) {
-        this.customer_no = customer_no;
-    }
-
-    @Override
     public Integer getSyncSendStatus() {
         return syncSendStatus;
     }
 
-    @Override
     public void setSyncSendStatus(Integer syncSendStatus) {
         this.syncSendStatus = syncSendStatus;
+    }
+
+    public String getTplPkgStatus() {
+        return tplPkgStatus;
+    }
+
+    public void setTplPkgStatus(String tplPkgStatus) {
+        this.tplPkgStatus = tplPkgStatus;
+    }
+
+    public Double getFreight() {
+        return freight;
+    }
+
+    public void setFreight(Double freight) {
+        this.freight = freight;
+    }
+
+    public Double getSkuWeight() {
+        return skuWeight;
+    }
+
+    public void setSkuWeight(Double skuWeight) {
+        this.skuWeight = skuWeight;
+    }
+
+    public String getMallOrders() {
+        return mallOrders;
+    }
+
+    public void setMallOrders(String mallOrders) {
+        this.mallOrders = mallOrders;
+    }
+
+    public String getShippingPrinter() {
+        return shippingPrinter;
+    }
+
+    public void setShippingPrinter(String shippingPrinter) {
+        this.shippingPrinter = shippingPrinter;
+    }
+
+    public String getCompanyNo() {
+        return companyNo;
+    }
+
+    public void setCompanyNo(String companyNo) {
+        this.companyNo = companyNo;
+    }
+
+    public String getGlsReturnBack() {
+        return glsReturnBack;
+    }
+
+    public void setGlsReturnBack(String glsReturnBack) {
+        this.glsReturnBack = glsReturnBack;
+    }
+
+    public String getReceiverState() {
+        return receiverState;
+    }
+
+    public void setReceiverState(String receiverState) {
+        this.receiverState = receiverState;
+    }
+
+    public String getReceiverCity() {
+        return receiverCity;
+    }
+
+    public void setReceiverCity(String receiverCity) {
+        this.receiverCity = receiverCity;
+    }
+
+    public String getReceiverDistrict() {
+        return receiverDistrict;
+    }
+
+    public void setReceiverDistrict(String receiverDistrict) {
+        this.receiverDistrict = receiverDistrict;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public void setMemo(String memo) {
+        this.memo = memo;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public String getIdCardBack() {
+        return idCardBack;
+    }
+
+    public void setIdCardBack(String idCardBack) {
+        this.idCardBack = idCardBack;
+    }
+
+    public String getIdCardFront() {
+        return idCardFront;
+    }
+
+    public void setIdCardFront(String idCardFront) {
+        this.idCardFront = idCardFront;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public String getModifier() {
+        return modifier;
+    }
+
+    public void setModifier(String modifier) {
+        this.modifier = modifier;
+    }
+
+    public String getLogisticType() {
+        return logisticType;
+    }
+
+    public void setLogisticType(String logisticType) {
+        this.logisticType = logisticType;
+    }
+
+    public String getSalesId() {
+        return salesId;
+    }
+
+    public void setSalesId(String salesId) {
+        this.salesId = salesId;
     }
 }
