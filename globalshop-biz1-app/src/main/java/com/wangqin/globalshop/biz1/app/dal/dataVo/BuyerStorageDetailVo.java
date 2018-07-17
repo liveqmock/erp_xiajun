443e98c022e1b86b98c59610ce7024768416368b
zyfjxust@163.com <zyfjxust@163.com>
2018-07-17 14:57:01 +0800

    入库单，新增实际入库数，在已入库明细中展示

--- globalshop-biz1-app/src/main/java/com/wangqin/globalshop/biz1/app/dal/dataVo/BuyerStorageDetailVo.java
+++ globalshop-biz1-app/src/main/java/com/wangqin/globalshop/biz1/app/dal/dataVo/BuyerStorageDetailVo.java
@@ -21,9 +21,11 @@ public class BuyerStorageDetailVo {
 
 	private String specifications;//规格
 
-	private Integer quantity; //小程序端，这个是线下数量，现在它和globalshop前端，查询后置=BuyerStorageDetailDo.quantity+BuyerStorageDetailDo.transQuantity
+	private Integer quantity; //小程序端，这个是线下数量，
 
-	private Integer transQuantity; //在途，预入库数，小程序段，这里是在途数量，现在是前端置为BuyerStorageDetailDo.quantity+BuyerStorageDetailDo.transQuantity
+	private Integer transQuantity; //在途，预入库数，
+
+	private Integer entryQuantity; //实际入库数=默认值=在途+线下=客户手工填写入库数
 
 	private String buyerName; //买手名字
 
@@ -57,7 +59,12 @@ public class BuyerStorageDetailVo {
 
 	private String buyerTaskNo;
 
-
+	public Integer getEntryQuantity() {
+		return entryQuantity;
+	}
+	public void setEntryQuantity(Integer entryQuantity) {
+		this.entryQuantity = entryQuantity;
+	}
 	public String getBuyerTaskNo() {
 		return buyerTaskNo;
 	}
