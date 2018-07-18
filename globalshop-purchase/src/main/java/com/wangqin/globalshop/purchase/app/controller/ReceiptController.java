665eefa66fabfb18fc6be5aa27ccb9f23fc12afa
Angus Liu <anguang.liu@buyer007.com>
2018-07-18 14:10:31 +0800

    采购小票管理部分查询功能恢复

--- globalshop-purchase/src/main/java/com/wangqin/globalshop/purchase/app/controller/ReceiptController.java
+++ globalshop-purchase/src/main/java/com/wangqin/globalshop/purchase/app/controller/ReceiptController.java
@@ -22,8 +22,7 @@ import java.util.List;
 @Controller
 @Authenticated
 @ResponseBody
-@RequestMapping(value = {
-        "/receipt"})
+@RequestMapping(value = {"/receipt"})
 public class ReceiptController {
     @Autowired
     private IReceiptService service;
@@ -41,10 +40,11 @@ public class ReceiptController {
         }
         return result.buildIsSuccess(true).buildData(list);
     }
+
     @RequestMapping("/queryTaskReceipt")
     public Object queryReceiptDetail(BuyerReceiptDetailDO receipt){
         JsonResult<List<BuyerReceiptDetailVo>> result = new JsonResult<>();
-        List<BuyerReceiptDetailVo> list = null;
+        List<BuyerReceiptDetailVo> list;
         try {
             receipt.setCompanyNo(AppUtil.getLoginUserCompanyNo());
             list = detailService.getVoList(receipt);
