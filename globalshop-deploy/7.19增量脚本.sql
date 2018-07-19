74bebe6aed3c1c35be3c1142874b71bfb6cf1d44
zyfjxust@163.com <zyfjxust@163.com>
2018-07-19 16:53:08 +0800

    item_find新增主图

--- globalshop-deploy/7.19增量脚本.sql
+++ globalshop-deploy/7.19增量脚本.sql
@@ -7,3 +7,7 @@ ADD COLUMN `discount` double DEFAULT NULL,
 ADD COLUMN `discount1` double DEFAULT NULL,
 ADD COLUMN `discount2` double DEFAULT NULL,
 ADD COLUMN `discount3` double DEFAULT NULL;
+
+# 增加主图字段
+ALTER TABLE `haidb2new`.`item_find`
+ADD COLUMN `main_pic` varchar(2048) ;
