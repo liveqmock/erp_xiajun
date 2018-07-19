fbf67d252fc26ddbac5e69d0903e351354c22bdd
zyfjxust@163.com <zyfjxust@163.com>
2018-07-19 17:04:08 +0800

    增加消息字段长度

--- globalshop-deploy/7.19增量脚本.sql
+++ globalshop-deploy/7.19增量脚本.sql
@@ -11,3 +11,7 @@ ADD COLUMN `discount3` double DEFAULT NULL;
 # 增加主图字段
 ALTER TABLE `haidb2new`.`item_find`
 ADD COLUMN `main_pic` varchar(2048) ;
+
+# 增加字段长度
+ALTER TABLE `haidb2new`.`db_migrate_send_record`
+MODIFY COLUMN `db_script` varchar(6144) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL ;
