8552bccd5861016fe8b62be98319ad4a23cb88de
biscuits <435866911@qq.com>
2018-07-19 09:48:08 +0800

    授权url

--- globalshop-usercenter-app/src/main/java/com/wangqin/globalshop/usercenter/controller/Wechat3rdPartyAuthorization.java
+++ globalshop-usercenter-app/src/main/java/com/wangqin/globalshop/usercenter/controller/Wechat3rdPartyAuthorization.java
@@ -6,10 +6,7 @@ import com.wangqin.globalshop.biz1.app.Exception.ErpCommonException;
 import com.wangqin.globalshop.biz1.app.aop.annotation.Authenticated;
 import com.wangqin.globalshop.biz1.app.dal.dataObject.AppletConfigDO;
 import com.wangqin.globalshop.common.redis.Cache;
-import com.wangqin.globalshop.common.utils.AppUtil;
-import com.wangqin.globalshop.common.utils.EasyUtil;
-import com.wangqin.globalshop.common.utils.HttpClientUtil;
-import com.wangqin.globalshop.common.utils.StringUtils;
+import com.wangqin.globalshop.common.utils.*;
 import com.wangqin.globalshop.common.utils.WxPay.PayUtil;
 import com.wangqin.globalshop.usercenter.service.IAppletConfigService;
 import com.wangqin.globalshop.usercenter.wechat_sdk.AesException;
@@ -109,7 +106,6 @@ public class Wechat3rdPartyAuthorization {
     }
 
 
-
     @RequestMapping("/getHtml")
     @Authenticated
     public String getHtml() {
@@ -141,6 +137,38 @@ public class Wechat3rdPartyAuthorization {
 
 
     }
+
+
+    @RequestMapping("/getAuthUrl")
+    @Authenticated
+    public Object getAuthUrl() {
+        JsonResult<Object> result = new JsonResult<>();
+        String re_url;
+        try {
+            String token = getToken();
+            log.info("获取小程序授权二维码:token===" + token);
+            String url = "https://api.weixin.qq.com/cgi-bin/component/api_create_preauthcode?component_access_token=" + token;
+            //language=JSON
+            String param = "{\"component_appid\":\"" + componentAppid + "\"}";
+            String post = PayUtil.httpRequest(url, "POST", param);
+            log.info("获取预授权码结果:" + post);
+            JSONObject object = JSON.parseObject(post);
+            /**预授权码*/
+            String preAuthCode = object.getString("pre_auth_code");
+            log.info("预授权码:" + preAuthCode);
+            //todo 配置的是http://test.buyer007.cn/account/queryAuth 微信文档显示 该回调地址必须是http  把 test.buyer007写到配置文件里面去
+            re_url = URLEncoder.encode("http://tests.buyer007.cn/account/authcallback", "UTF-8");
+            String reUrl = "https://mp.weixin.qq.com/cgi-bin/componentloginpage?component_appid=" + componentAppid + "&pre_auth_code=" + preAuthCode + "&redirect_uri=" + re_url + "&auth_type=2";
+            log.info("re_url:" + reUrl);
+            return result.buildIsSuccess(true).buildData(reUrl);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return result.buildIsSuccess(true).buildMsg("获取授权页面失败");
+
+
+    }
+
 
     /**
      * 根据auth_code查询授权信息
@@ -190,10 +218,10 @@ public class Wechat3rdPartyAuthorization {
         System.out.println(post2);
 
 
-
         return "";
 
     }
+
     //todo 设置小程序的其他信息
     @PostMapping("/B")
     @Authenticated
@@ -244,7 +272,7 @@ public class Wechat3rdPartyAuthorization {
         String url2 = "https://api.weixin.qq.com/wxa/get_page?access_token=" + authorizerAccessToken;
         String s2 = HttpClientUtil.get(url2);
         System.out.println(s2);
- //{"errcode":0,"errmsg":"ok","page_list":["pages\/index\/index","pages\/index\/webView","pages\/index\/special","pages\/order\/detail","pages\/order\/list","pages\/item\/list","pages\/user\/main","pages\/user\/about","pages\/user\/service","pages\/user\/address","pages\/user\/addAddress","pages\/user\/editAddress","pages\/cart\/list","pages\/cart\/lists","pages\/order\/allExpress","pages\/order\/preview","pages\/item\/detail"]}
+        //{"errcode":0,"errmsg":"ok","page_list":["pages\/index\/index","pages\/index\/webView","pages\/index\/special","pages\/order\/detail","pages\/order\/list","pages\/item\/list","pages\/user\/main","pages\/user\/about","pages\/user\/service","pages\/user\/address","pages\/user\/addAddress","pages\/user\/editAddress","pages\/cart\/list","pages\/cart\/lists","pages\/order\/allExpress","pages\/order\/preview","pages\/item\/detail"]}
 
         //todo 根据前两个api的返回结果  和 前段参数 拼装成 param参数
         String url3 = "https://api.weixin.qq.com/wxa/submit_audit?access_token=" + authorizerAccessToken;
@@ -291,11 +319,11 @@ public class Wechat3rdPartyAuthorization {
                     "\"authorizer_appid\":\"" + appid + "\"," +
                     "\"authorizer_refresh_token\":\"" + refreshToken + "\"" +
                     "}";
-            log.info("============appid->"+appid+"=================");
-            log.info("param================="+param);
+            log.info("============appid->" + appid + "=================");
+            log.info("param=================" + param);
             String post = PayUtil.httpRequest(url, "POST", param);
-            log.info("回调结果==============="+post);
-            log.info("============appid<-"+appid+"=================");
+            log.info("回调结果===============" + post);
+            log.info("============appid<-" + appid + "=================");
             JSONObject object = JSON.parseObject(post);
             applet.setAuthorizerRefreshToken(object.getString("authorizer_refresh_token"));
             applet.setAuthorizerAccessToken(object.getString("authorizer_access_token"));
@@ -311,6 +339,7 @@ public class Wechat3rdPartyAuthorization {
 
 //    11_E-AmSYNCYcn-HjhDMboOnVZ3afXdNKZ_0GPmrZYE7od728dFJJu1DKVF2u4IB83310RCNZ1aFyK4ecduBqlY5jLUOql3LZbET5NJP612kiLblFa5qedDPUhNjfZdhCCett8J8iFlXP7GFB6oVEWaAFDLUM
     }
+
     /***
      * 获取各个审核中通过的
      */
@@ -320,7 +349,7 @@ public class Wechat3rdPartyAuthorization {
         //todo 遍历相关数据库  拿到所有审核中的小程序的 auditid 和相应的 authorizer_access_token
 
         String accessToken = "";
-        String url = "https://api.weixin.qq.com/wxa/get_auditstatus?access_token="+accessToken;
+        String url = "https://api.weixin.qq.com/wxa/get_auditstatus?access_token=" + accessToken;
         //todo
         //language=JSON
 //        String param ="{\"auditid\":\"430371682\"}";
--- globalshop-usercenter-app/src/main/java/com/wangqin/globalshop/usercenter/controller/WechatLoginController.java
+++ globalshop-usercenter-app/src/main/java/com/wangqin/globalshop/usercenter/controller/WechatLoginController.java
@@ -22,7 +22,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
