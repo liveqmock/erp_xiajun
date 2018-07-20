6a5d1853d3bcfccc01cd483949d90d6292119644
biscuits <435866911@qq.com>
2018-07-20 09:59:36 +0800

    fix商城小程序后端无法获取保存在服务端缓存中的token的bug 1

--- globalshop-usercenter-app/src/main/java/com/wangqin/globalshop/usercenter/controller/Wechat3rdPartyAuthorization.java
+++ globalshop-usercenter-app/src/main/java/com/wangqin/globalshop/usercenter/controller/Wechat3rdPartyAuthorization.java
@@ -456,11 +456,8 @@ public class Wechat3rdPartyAuthorization {
     @RequestMapping("getInfo")
     public String getInfo() {
         String componentAccessToken = getToken();
-        String componentVerifyTicket = (String) loginCache.get("componentVerifyTicket");
-        Map<String, String> map = new HashMap<>();
-        map.put("componentAccessToken", componentAccessToken);
-        map.put("componentVerifyTicket", componentVerifyTicket);
-        return JSON.toJSONString(map);
+        log.info("小程序获取token===="+componentAccessToken);
+        return componentAccessToken;
 
     }
 
