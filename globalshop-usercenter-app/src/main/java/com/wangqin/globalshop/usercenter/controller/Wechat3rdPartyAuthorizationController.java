d251a5697f48c343e5de5499f5523a989819ccf2
biscuits <435866911@qq.com>
2018-07-23 11:24:14 +0800

    第三方授权微信业务闭环测试3

--- globalshop-usercenter-app/src/main/java/com/wangqin/globalshop/usercenter/controller/Wechat3rdPartyAuthorizationController.java
+++ globalshop-usercenter-app/src/main/java/com/wangqin/globalshop/usercenter/controller/Wechat3rdPartyAuthorizationController.java
@@ -218,6 +218,8 @@ public class Wechat3rdPartyAuthorizationController {
             log.info("最终小程序信息=======" + applet);
             appletConfigServiceImplement.insert(applet);
             return "success";
+        } catch (ErpCommonException e) {
+            return e.getErrorMsg();
         } catch (Exception e) {
             e.printStackTrace();
             return "fail";
@@ -285,17 +287,17 @@ public class Wechat3rdPartyAuthorizationController {
         PayUtil.httpRequest(url.replace("${token}", applet.getAuthorizerAccessToken()), "POST", param.replace("${extJson}", trueJson));
 
         String s = HttpClientUtil.get(imgUrl.replace("${token}", applet.getAuthorizerAccessToken()));
-        log.info("体验版二维码(旧)"+s);
+        log.info("体验版二维码(旧)" + s);
         String img;
         try (ByteArrayInputStream tInputStringStream = new ByteArrayInputStream(s.getBytes())) {
             img = uploadFileService.uploadImg(tInputStringStream, applet.getAppid() + str);
-            log.info("体验版二维码(新)"+s);
+            log.info("体验版二维码(新)" + s);
         }
         applet.setImgUrl(img);
         applet.setTempletId(templateId);
         applet.setPublishStatus(PubilshStatus.SUBMITTED.getCode());
         applet.setExtJson(trueJson);
-        log.info("发布体验版后的小程序"+applet);
+        log.info("发布体验版后的小程序" + applet);
 
     }
 
@@ -318,10 +320,11 @@ public class Wechat3rdPartyAuthorizationController {
         return null;
     }
 
-    public void auditApplet(AppletConfigDO applet) {
+    public void auditApplet(AppletConfigDO applet) throws ErpCommonException {
         String authorizerAccessToken = applet.getAuthorizerAccessToken();
         String url1 = "https://api.weixin.qq.com/wxa/get_category?access_token=" + authorizerAccessToken;
         String s1 = HttpClientUtil.get(url1);
+        log.info("获取分类结果" + s1);
         JSONObject o1 = JSON.parseObject(s1);
         if (!"ok".equals(o1.getString("errmsg"))) {
             throw new ErpCommonException("获取用户小程序分类失败");
@@ -330,16 +333,23 @@ public class Wechat3rdPartyAuthorizationController {
         String secondClass = o1.getString("second_class");
         String firstId = o1.getString("first_id");
         String secondId = o1.getString("second_id");
+        log.info("firstClass" + firstClass);
+        log.info("secondClass" + secondClass);
+        log.info("firstId" + firstId);
+        log.info("secondId" + secondId);
+
 
 //        {"errcode":0,"errmsg":"ok","category_list":[{"first_class":"商家自营","second_class":"海淘","first_id":304,"second_id":784}]}
 
         String url2 = "https://api.weixin.qq.com/wxa/get_page?access_token=" + authorizerAccessToken;
         String s2 = HttpClientUtil.get(url2);
+        log.info("获取页面结果" + s2);
         JSONObject o2 = JSON.parseObject(s2);
         if (!"ok".equals(o2.getString("errmsg"))) {
             throw new ErpCommonException("获取小程序主页失败");
         }
         JSONArray pageList = o2.getJSONArray("page_list");
+        log.info("pageList" + pageList);
         if (pageList.isEmpty()) {
             throw new ErpCommonException("小程序主页为空");
         }
@@ -352,12 +362,12 @@ public class Wechat3rdPartyAuthorizationController {
         String post3 = PayUtil.httpRequest(url3, "POST", param3);
 //        {"errcode":0,"errmsg":"ok","auditid":430371682}
         JSONObject obj3 = JSON.parseObject(post3);
-        log.info("提交审核的回调"+obj3.toJSONString());
+        log.info("提交审核的回调" + obj3.toJSONString());
         String auditid = obj3.getString("auditid");
-        log.info("auditid"+auditid);
+        log.info("auditid" + auditid);
         applet.setAuditId(auditid);
         applet.setPublishStatus(PubilshStatus.PENDING_REVIEW.getCode());
-        log.info("提交审核之后的"+applet);
+        log.info("提交审核之后的" + applet);
     }
 
 
