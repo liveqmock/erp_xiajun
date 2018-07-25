f92acd1a570d47edbe12245affcbce142cb7c64e
johnnyxiong <xiongjy2104@gmail.com>
2018-07-25 17:48:36 +0800

    update后返回true

--- globalshop-order-app/src/main/java/com/wangqin/globalshop/order/app/controller/mall/MallOrderController.java
+++ globalshop-order-app/src/main/java/com/wangqin/globalshop/order/app/controller/mall/MallOrderController.java
@@ -118,7 +118,7 @@ public class MallOrderController {
             return result.buildMsg(e.getMessage()).buildIsSuccess(false);
         }
 
-        return result;
+        return result.buildIsSuccess(true).buildMsg("更新成功");
     }
 
     /**
