be78760c013dd2e538eaa26d2ce4dd6d7f38273c
fanjun <fanjun@taofen8.com>
2018-07-25 09:47:31 +0800

    add job test

---
+++ globalshop-schedule-app/src/main/java/com/wangqin/globalshop/schedule/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.wangqin.globalshop.schedule.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping(value = "/api/schedule/test", method = RequestMethod.GET)
+    @ResponseBody
+    public String job(){
+        return "hello schedule" ;
+    }
+
+
+}
