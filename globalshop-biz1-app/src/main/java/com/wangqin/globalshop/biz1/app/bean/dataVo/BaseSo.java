0dfba0ac4635ea5488483ee22d6eb2a6d51df2fa
Angus Liu <anguang.liu@buyer007.com>
2018-07-27 17:33:59 +0800

    调整包结构，建议更新本地代码

---
+++ globalshop-biz1-app/src/main/java/com/wangqin/globalshop/biz1/app/bean/dataVo/BaseSo.java
@@ -0,0 +1,27 @@
+package com.wangqin.globalshop.biz1.app.bean.dataVo;
+
+import java.io.Serializable;
+
+/**
+ * Create by 777 on 2018/5/24
+ */
+public class BaseSo implements Serializable {
+
+	private int currentPage = 1;
+
+	private int pageSize = 100;
+
+	public int getCurrentPage() {
+		return currentPage;
+	}
+	public void setCurrentPage(int currentPage) {
+		this.currentPage = currentPage;
+	}
+	public int getPageSize() {
+		return pageSize;
+	}
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+
+}
