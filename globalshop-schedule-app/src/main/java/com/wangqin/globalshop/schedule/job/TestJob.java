b699ffc430cfb47ff994340287802896825cfb31
brucexiajun <brucexiajun@126.com>
2018-07-30 16:36:45 +0800

    增加定时任务：扫描商品表，判断可售状态

--- globalshop-schedule-app/src/main/java/com/wangqin/globalshop/schedule/job/TestJob.java
+++ globalshop-schedule-app/src/main/java/com/wangqin/globalshop/schedule/job/TestJob.java
@@ -1,11 +1,24 @@
 package com.wangqin.globalshop.schedule.job;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import com.wangqin.globalshop.biz1.app.dal.dataObject.ItemDO;
+import com.wangqin.globalshop.common.enums.ItemIsSale;
+import com.wangqin.globalshop.common.utils.DateUtil;
+import com.wangqin.globalshop.common.utils.IsEmptyUtil;
+import com.wangqin.globalshop.schedule.service.ScheduleItemService;
 
 @Component
 public class TestJob {
 
+	@Autowired
+	private ScheduleItemService scheduleItemService;
+	 
     @Scheduled(cron = "0/30 * * * * ? ")
     public void testJob(){
         System.out.println("========Test Job==========");
---
+++ globalshop-schedule-app/src/main/java/com/wangqin/globalshop/schedule/service/ScheduleItemService.java
@@ -0,0 +1,12 @@
+package com.wangqin.globalshop.schedule.service;
+
+import java.util.List;
+
+import com.wangqin.globalshop.biz1.app.dal.dataObject.ItemDO;
+
+public interface ScheduleItemService {
+
+	List<ItemDO> queryItemSalable(Byte isSale);
+	
+	void updateItemIsSale(Byte isSale,String itemCode);
+}
