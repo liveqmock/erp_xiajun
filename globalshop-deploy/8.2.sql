8ebfe3fcc5abc690ea332c3cc129d344141d1c36
brucexiajun <brucexiajun@126.com>
2018-07-31 10:37:52 +0800

    更新8.2.sql

--- globalshop-deploy/8.2.sql
+++ globalshop-deploy/8.2.sql
@@ -1,15 +1,15 @@
-#在item_sku表增加sku_rate字段
-ALTER TABLE `item_sku` ADD COLUMN `sku_rate` DOUBLE(10,2) NOT NULL DEFAULT '0' COMMENT '代理佣金比例';
+
+ALTER TABLE `item_sku` ADD COLUMN `sku_rate` DOUBLE(10,2) NOT NULL DEFAULT '0' COMMENT '浠ｇ悊浣ｉ噾姣斾緥';
 
 
-#新建分佣申请表mall_commision_apply
+
 CREATE TABLE IF NOT EXISTS `mall_commision_apply` (
   `id` bigint(64) NOT NULL AUTO_INCREMENT,
   `company_no` varchar(64) DEFAULT NULL,
   `order_no` varchar(64) DEFAULT NULL,
   `sub_order_no` varchar(64) DEFAULT NULL,
   `commision` varchar(4096) DEFAULT NULL,
-  `status` varchar(2) DEFAULT NULL COMMENT '0-新建; 1-订单签收确认; 2-清分',
+  `status` varchar(2) DEFAULT NULL COMMENT '0-鏂板缓; 1-璁㈠崟绛炬敹纭; 2-娓呭垎',
   `order_time` varchar(64) DEFAULT NULL,
   `order_date` datetime DEFAULT CURRENT_TIMESTAMP,
   `share_user_id` varchar(64) DEFAULT NULL,
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS `mall_commision_apply` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-#订单快照表mall_sub_order_snapshot
+
 CREATE TABLE IF NOT EXISTS `mall_sub_order_snapshot` (
   `id` bigint(64) NOT NULL AUTO_INCREMENT,
   `company_no` varchar(64) DEFAULT NULL,
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `mall_sub_order_snapshot` (
   `sku_code` varchar(64) DEFAULT NULL,
   `sku_pic` varchar(512) DEFAULT NULL,
   `upc` varchar(64) DEFAULT NULL,
-  `scale` varchar(128) DEFAULT NULL COMMENT '规格，用分号隔开',
+  `scale` varchar(128) DEFAULT NULL COMMENT '瑙勬牸锛岀敤鍒嗗彿闅斿紑',
   `ext` varchar(4096) DEFAULT NULL,
   `sale_price` decimal(16,2) DEFAULT NULL,
   `is_del` tinyint(1) DEFAULT '0',
