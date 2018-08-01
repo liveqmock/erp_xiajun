3bedff375376559cce8fb3b43b4966c409b2fdef
brucexiajun <brucexiajun@126.com>
2018-08-01 15:12:20 +0800

    提供统计接口

---
+++ globalshop-schedule-app/src/main/java/com/wangqin/globalshop/schedule/service/CommissionSumaryDetailService.java
@@ -0,0 +1,9 @@
+package com.wangqin.globalshop.schedule.service;
+
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
+
+public interface CommissionSumaryDetailService {
+	Map<String,Object> sumOrderNumSaleCommissionByUserNo(Integer status,String userNo);
+}
