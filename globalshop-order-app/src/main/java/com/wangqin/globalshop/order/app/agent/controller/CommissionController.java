c18523cb91d86f92ffeafc655979dc0ef8f809de
brucexiajun <brucexiajun@126.com>
2018-08-02 15:25:46 +0800

    添加图片字段

--- globalshop-order-app/src/main/java/com/wangqin/globalshop/order/app/agent/controller/CommissionController.java
+++ globalshop-order-app/src/main/java/com/wangqin/globalshop/order/app/agent/controller/CommissionController.java
@@ -58,7 +58,7 @@ public class CommissionController {
 	    		return result.buildFailed("代理不存在");
 	    	}
 	    	AgentCommissionVO agentCommissionVO = new AgentCommissionVO();
-	    	agentCommissionVO.setStatus(qv.getS);
+	    	agentCommissionVO.setStatus(qv.getStatus());
 	    	agentCommissionVO.setUserNo(agentNo);
 	    	agentCommissionVO.setProfile(agentDO.getHeadProtraitUrl());
 	    	agentCommissionVO.setName(agentDO.getAgentName());
