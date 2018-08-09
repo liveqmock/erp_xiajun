13f0f75fee7290581bf86e4a32331aa703445f66
Angus Liu <anguang.liu@buyer007.com>
2018-08-09 17:14:58 +0800

    修改：处理 company 返回的 logo url

--- globalshop-mall/src/main/java/com/wangqin/globalshop/company/service/impl/CompanyServiceImpl.java
+++ globalshop-mall/src/main/java/com/wangqin/globalshop/company/service/impl/CompanyServiceImpl.java
@@ -7,10 +7,7 @@ import com.wangqin.globalshop.biz1.app.bean.dataVo.PageQueryParam;
 import com.wangqin.globalshop.biz1.app.dal.dataObject.*;
 import com.wangqin.globalshop.biz1.app.dal.mapperExt.CompanyDOMapperExt;
 import com.wangqin.globalshop.biz1.app.exception.BizCommonException;
-import com.wangqin.globalshop.common.utils.AppUtil;
-import com.wangqin.globalshop.common.utils.CodeGenUtil;
-import com.wangqin.globalshop.common.utils.Md5Util;
-import com.wangqin.globalshop.common.utils.StringUtil;
+import com.wangqin.globalshop.common.utils.*;
 import com.wangqin.globalshop.company.service.*;
 import com.wangqin.globalshop.item.app.service.IAppletConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -87,7 +84,9 @@ public class CompanyServiceImpl implements CompanyService {
         if (companyNo == null) {
             throw new BizCommonException("信息不完整！");
         }
-        return companyDOMapper.selectByCompanyNo(companyNo);
+        CompanyDO companyDO = companyDOMapper.selectByCompanyNo(companyNo);
+        companyDO.setLogoUrl(ImgUtil.initImg2Url(companyDO.getLogoUrl()));
+        return companyDO;
     }
 
     @Override
