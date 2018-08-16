3da957ffbd6b386650b4c6345bcd456e038d8373
johnnyxiong <xiongjy2104@gmail.com>
2018-08-16 22:26:01 +0800

    暂去掉后端权限，因有问题

--- globalshop-mall/src/main/java/com/wangqin/globalshop/company/aop/CompanyOptAspect.java
+++ globalshop-mall/src/main/java/com/wangqin/globalshop/company/aop/CompanyOptAspect.java
@@ -14,26 +14,26 @@ import org.springframework.stereotype.Component;
  * @author angus
  * @date 2018/8/9
  */
-@Aspect
-@Component
+//@Aspect
+//@Component
 public class CompanyOptAspect {
 
-    @Pointcut("execution(* com.wangqin.globalshop.company.controller.CompanyController.*(..))")
-    public void authenticate() {
-    }
-
-    @Around("authenticate()")
-    public Object doBefore(ProceedingJoinPoint pjp) throws Throwable {
-        // 商家管理粗略的权限认证
-        String legalCompanyNo = "-1";
-        String legalUserId = "sysadmin";
-        String loginUserCompanyNo = AppUtil.getLoginUserCompanyNo();
-        String loginUserId = AppUtil.getLoginUserId();
-        if (!legalCompanyNo.equals(loginUserCompanyNo) || !legalUserId.equals(loginUserId)) {
-            JsonResult result = new JsonResult();
-            result.buildIsSuccess(false).buildMsg("没有权限");
-            return result;
-        }
-        return pjp.proceed();
-    }
+//    @Pointcut("execution(* com.wangqin.globalshop.company.controller.CompanyController.*(..))")
+//    public void authenticate() {
+//    }
+//
+//    @Around("authenticate()")
+//    public Object doBefore(ProceedingJoinPoint pjp) throws Throwable {
+//        // 商家管理粗略的权限认证
+//        String legalCompanyNo = "-1";
+//        String legalUserId = "sysadmin";
+//        String loginUserCompanyNo = AppUtil.getLoginUserCompanyNo();
+//        String loginUserId = AppUtil.getLoginUserId();
+//        if (!legalCompanyNo.equals(loginUserCompanyNo) || !legalUserId.equals(loginUserId)) {
+//            JsonResult result = new JsonResult();
+//            result.buildIsSuccess(false).buildMsg(""+loginUserCompanyNo+","+loginUserId+"没有权限");
+//            return result;
+//        }
+//        return pjp.proceed();
+//    }
 }
