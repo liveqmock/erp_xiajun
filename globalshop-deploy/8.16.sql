3fec5b25e864e268bb7d696f166d1b34f660d821
johnnyxiong <xiongjy2104@gmail.com>
2018-08-20 15:32:25 +0800

    合并增量库

--- globalshop-deploy/8.16.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE applet_config ADD requestdomain VARCHAR(256) NULL COMMENT 'request合法域名';
-ALTER TABLE applet_config ADD wsrequestdomain VARCHAR(256) NULL COMMENT 'socket合法域名';
-ALTER TABLE applet_config ADD uploaddomain VARCHAR(256) NULL COMMENT 'uploadFile合法域名';
-ALTER TABLE applet_config ADD downloaddomain VARCHAR(256) NULL COMMENT 'downloadFile合法域名';
-ALTER TABLE applet_config ADD webviewdomain VARCHAR(256) NULL COMMENT '业务域名';
